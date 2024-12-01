21d2be5933069965792589f2e8f1dda66a521e68
KupperGm <gabrielkuppermorais@gmail.com>
2024-12-01 14:54:00 -0300

    inserindo valores as respostas

---
+++ script.js
@@ -0,0 +1,10 @@
+function testar(){
+    let contHomer=0;
+    let contMargie=0;
+    let contBart=0;
+    let contLisa=0;
+
+    let radios = document.querySelectorAll('input[type="radio"]')
+    console.log(radios)
+
+}
