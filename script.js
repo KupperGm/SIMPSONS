999b2bc4247a5784be5524bf8a4e7f52308d6a35
KupperGm <gabrielkuppermorais@gmail.com>
2024-12-01 15:01:14 -0300

    inciando a função de testar os valores

--- script.js
+++ script.js
@@ -7,4 +7,21 @@ function testar(){
     let radios = document.querySelectorAll('input[type="radio"]')
     console.log(radios)
 
+    radios.forEach(e =>{
+
+        if(e.checked) {
+            if(e.value == "Homer"){
+                contHomer++;
+            }
+            else if(e.value == "Margie"){
+                contMargie++;
+            }
+            else if(e.value == "Lisa"){
+                contLisa++;
+            }
+            else if(e.value == "Bart"){
+                contBart++;
+            }
+        }
+    })
 }
